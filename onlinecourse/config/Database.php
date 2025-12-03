968d501b69191b0e30df7f351a7355099d10aaf9
TranMinhGiang <2351160515@e.tlu.edu.vn>
2025-12-03 21:00:53 +0700

    Sua file Database.php

--- onlinecourse/config/Database.php
+++ onlinecourse/config/Database.php
@@ -1,10 +1,29 @@
 <?php
-// Placeholder Database connection class
-class Database
-{
-    public static function connect()
-    {
-        // TODO: return PDO or mysqli connection
-        return null;
+class Database {
+    private static $instance = null;
+    private $conn;
+
+    private $host = "localhost";
+    private $dbname = "online_course";
+    private $username = "root";
+    private $password = "";
+
+    // Constructor private để ngăn tạo nhiều kết nối
+    private function __construct() {
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->dbname};charset=utf8",
+                $this->username,
+                $this->password,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,   // Bật chế độ báo lỗi
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Kiểu fetch mặc định
+                    PDO::ATTR_EMULATE_PREPARES => false, // Tắt giả lập prepare để bảo mật hơn
+                ]
+            );
+        } catch (PDOException $e) {
+            die("Lỗi kết nối CSDL: " . $e->getMessage());
+        }
     }
 }
+?>
